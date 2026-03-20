fcbc16b009a8757f68dcebd9e84695427b16335f
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2026-03-20 08:44:55 +0800

    Add PipelineEntryDetail import to models init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .company import Company
 from .pipeline_entry import PipelineEntry
+from .pipeline_entry_detail import PipelineEntryDetail
 from .fee_standard import FeeStandard
 from .fee_record import FeeRecord
 from .contract import Contract
